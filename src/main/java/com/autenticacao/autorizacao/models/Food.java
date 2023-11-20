cb222cfb7db48a0764dd7b1b274012441aff4098
JEAN CARLOS CRISTÓVÃO <jean_cristovao@estudante.sesisenai.org.br>
2023-11-20 20:45:08 -0300

    Created Food model

---
+++ src/main/java/com/autenticacao/autorizacao/models/Food.java
@@ -0,0 +1,33 @@
+package com.autenticacao.autorizacao.models;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Table(name = "foods")
+@Entity(name = "foods")
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Food {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "VARCHAR(255)")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private UUID id;
+    private String title;
+    private String image;
+    private Integer price;
+}
