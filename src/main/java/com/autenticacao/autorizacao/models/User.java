64039b91e72f7d7017f0d4385db491b5a7353166
JEAN CARLOS CRISTÓVÃO <jean_cristovao@estudante.sesisenai.org.br>
2023-11-20 21:18:12 -0300

    Created User model

---
+++ src/main/java/com/autenticacao/autorizacao/models/User.java
@@ -0,0 +1,43 @@
+package com.autenticacao.autorizacao.models;
+
+import java.util.UUID;
+
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "users")
+@Entity(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(of = "id")
+public class User {
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", columnDefinition = "VARCHAR(255)")
+    @JdbcTypeCode(SqlTypes.VARCHAR)
+    private UUID id;
+    private String name;
+    private String email;
+    private String password;
+
+    public User(User data) {
+        this.name = data.getName();
+        this.email = data.getEmail();
+        this.password = data.getPassword();
+    }
+}
