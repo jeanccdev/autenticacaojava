b7ae393b53448fcdc6842cacce8e55d1478c7334
JEAN CARLOS CRISTÓVÃO <jean_cristovao@estudante.sesisenai.org.br>
2023-11-20 21:00:44 -0300

    Implemented get one route to food controller

--- src/main/java/com/autenticacao/autorizacao/controllers/FoodController.java
+++ src/main/java/com/autenticacao/autorizacao/controllers/FoodController.java
@@ -1,10 +1,13 @@
 package com.autenticacao.autorizacao.controllers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +26,12 @@ public class FoodController {
     public List<Food> getAll() {
         List<Food> foodList = repository.findAll();
         return foodList;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping("/one/{id}")
+    public Optional<Food> getOne(@PathVariable("id") UUID id) {
+        Optional<Food> food = repository.findById(id);
+        return food;
     }
 }
